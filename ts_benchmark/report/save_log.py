2359a55faf277dac50345457c8302666fe4471be
qiu69 <qiuxiangfei69@163.com>
2024-06-07 19:54:38 +0800

    modify save_log.py

--- ts_benchmark/report/save_log.py
+++ ts_benchmark/report/save_log.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
+import itertools
 import os
+import logging
 
 import pandas as pd
 
 from ts_benchmark.common.constant import ROOT_PATH
 from ts_benchmark.report.utils import write_log_file
 from ts_benchmark.utils.get_file_name import get_log_file_name
+
+# Configure the logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def save_log(
@@ -16,18 +22,22 @@ def save_log(
 ) -> str:
     """
     Save log data.
-
     Save the evaluation results, model hyperparameters, model evaluation configuration, and model name to a log file.
-
     :param save_path: saved path.
     :param result_df: dataframe of the evaluation result.
     :param model_name: model name
     :param compress_method: the compression method for the output file.
     """
-    if result_df["log_info"].values.any():
-        error_message = result_df["log_info"].values
-        for line in error_message:
-            print(line)
+
+    if result_df["log_info"].any():
+        error_itr = filter(None, result_df["log_info"])
+        for error in itertools.islice(error_itr, 3):
+            logger.info(error)
+        if any(error_itr):
+            logger.info(
+                "-------------More error messages can be found in the record files!-------------"
+            )
+
     if save_path is not None:
         result_path = os.path.join(ROOT_PATH, "result", save_path)
     else:
