0f907356c63f0dd1b4b9b2631a84be267e20ad89
pandajunkai <pandajunkai@163.com>
2025-06-21 16:14:18 +0800

    add new base model and modify all model adapter

--- ts_benchmark/baselines/utils.py
+++ ts_benchmark/baselines/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import copy
 from typing import Tuple
 
 import numpy as np
@@ -10,6 +11,60 @@ from ts_benchmark.baselines.time_series_library.utils.timefeatures import (
     time_features,
 )
 from ts_benchmark.utils.data_processing import split_time
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    # lr = args.learning_rate * (0.2 ** (epoch // 2))
+    if args.lradj == 'type1':
+        lr_adjust = {epoch: args.lr * (0.5 ** ((epoch - 1) // 1))}
+    elif args.lradj == 'type2':
+        lr_adjust = {
+            2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
+            10: 5e-7, 15: 1e-7, 20: 5e-8
+        }
+    elif args.lradj == 'type3':
+        lr_adjust = {epoch: args.lr if epoch < 3 else args.lr * (0.9 ** ((epoch - 3) // 1))}
+    elif args.lradj == 'constant':
+        lr_adjust = {epoch: args.lr}
+    if epoch in lr_adjust.keys():
+        lr = lr_adjust[epoch]
+        for param_group in optimizer.param_groups:
+            param_group['lr'] = lr
+        print('Updating learning rate to {}'.format(lr))
+
+
+class EarlyStopping:
+    def __init__(self, patience=7, delta=0):
+        self.patience = patience
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.check_point = None
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f"EarlyStopping counter: {self.counter} out of {self.patience}")
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        print(
+            f"Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ..."
+        )
+        self.check_point = copy.deepcopy(model.state_dict())
+        # self.check_point = model.state_dict()
+        self.val_loss_min = val_loss
 
 
 class SlidingWindowDataLoader:
