f1e3f7cf9e73f2d0568672a6498ca2e4bae9370d
PandaJunk <pandajunkai@163.com>
2026-01-15 06:54:29 +0000

    fix the bug from ModernTCN

--- ts_benchmark/baselines/moderntcn/moderntcn.py
+++ ts_benchmark/baselines/moderntcn/moderntcn.py
@@ -82,27 +82,27 @@ class ModernTCN(DeepForecastingModelBase):
                                             max_lr=self.config.lr)
         return criterion, optimizer
 
-    def adjust_learning_rate(optimizer, scheduler, epoch, args, printout=True):
+    def adjust_learning_rate(self, optimizer, scheduler, epoch, args, printout=True):
     # lr = args.learning_rate * (0.2 ** (epoch // 2))
         if args.lradj == 'type1':
-            lr_adjust = {epoch: args.learning_rate * (0.5 ** ((epoch - 1) // 1))}
+            lr_adjust = {epoch: args.lr * (0.5 ** ((epoch - 1) // 1))}
         elif args.lradj == 'type2':
             lr_adjust = {
                 2: 5e-5, 4: 1e-5, 6: 5e-6, 8: 1e-6,
                 10: 5e-7, 15: 1e-7, 20: 5e-8
             }
         elif args.lradj == 'type3':
-            lr_adjust = {epoch: args.learning_rate if epoch < 3 else args.learning_rate * (0.9 ** ((epoch - 3) // 1))}
+            lr_adjust = {epoch: args.lr if epoch < 3 else args.lr * (0.9 ** ((epoch - 3) // 1))}
         elif args.lradj == 'constant':
-            lr_adjust = {epoch: args.learning_rate}
+            lr_adjust = {epoch: args.lr}
         elif args.lradj == '3':
-            lr_adjust = {epoch: args.learning_rate if epoch < 10 else args.learning_rate*0.1}
+            lr_adjust = {epoch: args.lr if epoch < 10 else args.lr*0.1}
         elif args.lradj == '4':
-            lr_adjust = {epoch: args.learning_rate if epoch < 15 else args.learning_rate*0.1}
+            lr_adjust = {epoch: args.lr if epoch < 15 else args.lr*0.1}
         elif args.lradj == '5':
-            lr_adjust = {epoch: args.learning_rate if epoch < 25 else args.learning_rate*0.1}
+            lr_adjust = {epoch: args.lr if epoch < 25 else args.lr*0.1}
         elif args.lradj == '6':
-            lr_adjust = {epoch: args.learning_rate if epoch < 5 else args.learning_rate*0.1}  
+            lr_adjust = {epoch: args.lr if epoch < 5 else args.lr*0.1}  
         elif args.lradj == 'TST':
             lr_adjust = {epoch: scheduler.get_last_lr()[0]}
         
@@ -122,7 +122,7 @@ class ModernTCN(DeepForecastingModelBase):
         :param epoch: The current training epoch used to calculate the new learning rate.
         :param config: Configuration object containing parameters that control learning rate adjustment.
         """
-        self.adjust_lr(optimizer, self.scheduler, epoch, config)
+        self.adjust_learning_rate(optimizer, self.scheduler, epoch, config)
         self.scheduler.step()
     
     @property
